b9752152fa261b7642384a5c63441e69c1aa956d
luis25 <luis.chavez@alumno.udg.mx>
2013-12-10 18:37:14 -0600

    tarea usuario

---
+++ Usuario/Usuario/MainForm.cs
@@ -0,0 +1,95 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Text;
+
+namespace Usuario
+{
+
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+		
+		void PesosTextChanged(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void CambioTextChanged(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void DolarTextChanged(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void ConvertirClick(object sender, EventArgs e)
+		{
+			if ( Peso.Text.Length == 0 || Cambio.Text.Length == 0){
+				MessageBox.Show("Los campos no deben estar vacios",
+				                "Advertencia", MessageBoxButtons.OK);
+				return;
+			}
+			
+			
+			double dolar, peso, cambio;
+
+			dolar = System.Double.Parse(Peso.Text);
+			pesos = System.Double.Parse(Cambio.Text);
+			//Opreación
+			cambio = dolar / peso;
+			Dolar.Text = cambio.ToString();
+
+			
+		}
+		
+		
+		void PesoNum_keyPress(object sender, System.Windows.Forms.KeyPressEventArgs e)
+		{
+			if (!(char.IsNumber(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))
+			{
+				MessageBox.Show("Solo se permiten numeros", "Advertencia", 
+				                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+				e.Handled = true;
+				return;
+			}
+
+			
+		}
+
+		
+		void CambioNum_keyPress(object sender, KeyPressEventArgs e)
+		{
+			if (!(char.IsNumber(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))
+			{
+				MessageBox.Show("Solo se permiten numeros");
+				              
+				e.Handled = true;
+				return;
+			}
+
+		}
+		
+		void DolaresNum_keyPress(object sender, KeyPressEventArgs e)
+		{
+			if (!(char.IsNumber(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))
+			{
+				MessageBox.Show(" Advertencia solo se permiten números");
+				               
+				e.Handled = true;
+				return;
+			}
+
+			
+		}
+	}
+}
+
+
---
+++ Usuario/Usuario/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Windows.Forms;
+
+namespace Usuario
+{
+	
+	internal sealed class Program
+	{
+		
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
